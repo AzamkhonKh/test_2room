9b4a136c3cba2a9e324284866636d3f2260031bd
azamkhon <azamkhon.kh@gmail.com>
2022-05-11 10:32:12 +0500

    jwt: created middleware, routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\JWTController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('login',  [JWTController::class,'authenticate']);
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('user', [UserController::class,'index']);
 });
